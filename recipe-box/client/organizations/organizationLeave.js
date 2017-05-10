f47cfedbc87b2bfa1398de78370621311cba4877
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-05-10 00:32:45 +0000

    added session variable to leaveOrg template and allowed form to open on leave button

--- recipe-box/client/organizations/organizationLeave.js
+++ recipe-box/client/organizations/organizationLeave.js
@@ -9,6 +9,10 @@ Template.OrganizationLeave.onCreated(function() {
 Template.OrganizationLeave.events ({
     'click .fa-close': function () {
         Session.set('leaveOrganization', false);
+    },
+    'click .toggle-organization': function () {
+      Session.set('withdrawMembership', true);
+      var elementPosition = $(this).index();
     }
 });
 
